d1ef90c447a0d97725a8141d244eb79f8ba1da1e
Harald Nezbeda <hn@nezhar.com>
2018-04-15 22:07:08 +0200

    Replace select with ng-select

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { NgSelectModule } from '@ng-select/ng-select';
 import { PerfectScrollbarModule } from 'ngx-perfect-scrollbar';
 import { PERFECT_SCROLLBAR_CONFIG } from 'ngx-perfect-scrollbar';
 import { PerfectScrollbarConfigInterface } from 'ngx-perfect-scrollbar';
@@ -33,6 +35,8 @@ library.add(faCoffee, faCode, faSyncAlt, faGlobe, faEye, faEyeSlash, faStar, faT
     FontAwesomeModule,
     NgbModule.forRoot(),
     PerfectScrollbarModule,
+    NgSelectModule,
+    FormsModule,
   ],
   providers: [
     {
