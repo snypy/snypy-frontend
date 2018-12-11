8fc28efa62920cf86115fd0e027c528979aa33a0
Harald Nezbeda <hn@nezhar.com>
2018-12-11 20:13:39 +0100

    Fixes multiplication issue in structural directive

--- src/app/directives/perm/perm.directive.ts
+++ src/app/directives/perm/perm.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, Input, TemplateRef, ViewContainerRef } from '@angular/core';
+import { Directive, Input, TemplateRef, ViewContainerRef, OnChanges } from '@angular/core';
 import { ResourceModel } from "ngx-resource-factory/resource/resource-model";
 import { ROLES, UserTeam } from "../../services/resources/userteam.resource";
 import { Snippet } from "../../services/resources/snippet.resource";
@@ -17,47 +17,22 @@ export enum PERMISSION {
 @Directive({
   selector: '[appPerm]'
 })
-export class PermDirective {
+export class PermDirective implements OnChanges {
 
   permName: PERMISSION;
   permArgs: any[] = [];
   hasPerm: boolean;
 
-  constructor(
-    private templateRef: TemplateRef<any>,
-    private viewContainer: ViewContainerRef) {
-  }
-
-  @Input() set appPerm(permission: PERMISSION) {
-    this.permName = permission;
-  }
-
-  @Input() set appPermArgs(args: any[]) {
-    this.permArgs = args;
-    this.hasPerm = PermDirective.checkPerm(this.permName, this.permArgs);
-
-    if (this.hasPerm) {
-      this.viewContainer.createEmbeddedView(this.templateRef);
-    } else {
-      this.viewContainer.clear();
-    }
-  }
-
-  @Input() set appPermElse(templateRef: TemplateRef<any>) {
-    if (!this.hasPerm) {
-      this.viewContainer.createEmbeddedView(templateRef);
-    }
-  }
-
   static checkPerm(permName: PERMISSION, args: any[]): boolean {
     return PermDirective[permName](...args);
   }
+
 
   /**
    * Permissions
    */
   static cadAddTeamMember(userTeam: ResourceModel<UserTeam>): boolean {
-    return userTeam.role == ROLES.EDITOR;
+    return userTeam.role === ROLES.EDITOR;
   }
 
   static canEditTeamMember(userTeam: ResourceModel<UserTeam>): boolean {
@@ -86,5 +61,33 @@ export class PermDirective {
 
   static canDeleteSnippet(user: ResourceModel<User>, snippet: ResourceModel<Snippet>) {
     return PermDirective.canEditSnippet(user, snippet);
+  }
+
+  constructor(
+    private templateRef: TemplateRef<any>,
+    private viewContainer: ViewContainerRef) {
+  }
+
+  ngOnChanges() {
+    this.viewContainer.clear();
+
+    if (this.hasPerm) {
+      this.viewContainer.createEmbeddedView(this.templateRef);
+    }
+  }
+
+  @Input() set appPerm(permission: PERMISSION) {
+    this.permName = permission;
+  }
+
+  @Input() set appPermArgs(args: any[]) {
+    this.permArgs = args;
+    this.hasPerm = PermDirective.checkPerm(this.permName, this.permArgs);
+  }
+
+  @Input() set appPermElse(templateRef: TemplateRef<any>) {
+    if (!this.hasPerm) {
+      this.viewContainer.createEmbeddedView(templateRef);
+    }
   }
 }
